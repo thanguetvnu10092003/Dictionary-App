127cafd07855680e8bcdd5952681af8de252a201
LeToanThang <toanthangvietduc@gmail.com>
2024-04-09 22:11:39 +0700

    Create CommonConstants.java

---
+++ src/constants/CommonConstants.java
@@ -0,0 +1,15 @@
+package constants;
+
+import java.awt.*;
+
+public class CommonConstants {
+    // colors
+    public static final Color LIGHT_BLUE = Color.decode("#5699FD");
+    public static final Color DARK_BLUE = Color.decode("#45699E");
+    public static final Color BRIGHT_YELLOW = Color.decode("#B7FF58");
+    public static final Color LIGHT_RED = Color.decode("#FF8585");
+    public static final Color LIGHT_GREEN = Color.decode("#76F592");
+    public static final Color BLACK = Color.BLACK;
+    public static final Color PINK = Color.PINK;
+    public static final Color RED = Color.RED;
+}
