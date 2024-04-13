1f9cfffed56475d2873a4ce71386210c9051ac1e
LeToanThang <toanthangvietduc@gmail.com>
2024-04-13 12:12:00 +0700

    Update Main.java

--- src/base/Main.java
+++ src/base/Main.java
@@ -3,7 +3,7 @@ package base;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         MyJDBC.importDatabase();
         DictionaryCommandline.dictionaryAdvance();
     }
