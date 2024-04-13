c0a5f6b3be23762a7fa4e254ba85887b179b2fbb
LeToanThang <toanthangvietduc@gmail.com>
2024-04-13 12:11:58 +0700

    Update DictionaryManagement.java

--- src/base/DictionaryManagement.java
+++ src/base/DictionaryManagement.java
@@ -4,11 +4,15 @@ import java.io.*;
 import java.util.*;
 
 public class DictionaryManagement extends Dictionary{
+    private static String INPUT_PATH = "src/base/default.txt";
+    private static String PREV_INPUT_PATH = "";
+    private static String OUTPUT_PATH = "";
     public static String getInputPath() {
         return INPUT_PATH;
     }
 
     public static void setInputPath(String inputPath) {
+        PREV_INPUT_PATH = INPUT_PATH;
         INPUT_PATH = inputPath;
     }
 
@@ -20,8 +24,6 @@ public class DictionaryManagement extends Dictionary{
         OUTPUT_PATH = outputPath;
     }
 
-    private static String INPUT_PATH = "";
-    private static String OUTPUT_PATH = "";
     public static void insertFromCommandline() {
         Scanner sc_string  = new Scanner(System.in);
         Scanner sc_int  = new Scanner(System.in);
@@ -39,6 +41,7 @@ public class DictionaryManagement extends Dictionary{
                     oldWord.add(latest_word);
                 } else {
                     System.out.println("The word already exist");
+                    break;
                 }
             } else {
                 System.out.println("Enter its meaning: ");
@@ -46,8 +49,8 @@ public class DictionaryManagement extends Dictionary{
                 Word latest_word = new Word(input, meaning);
                 oldWord.add(latest_word);
             }
-
         }
+        Collections.sort(oldWord);
     }
 
     public static void removeDuplicates() {
@@ -67,16 +70,28 @@ public class DictionaryManagement extends Dictionary{
 
     public static void insertFromFile() {
         try {
+            BufferedReader reader1 = new BufferedReader(new FileReader(new File(PREV_INPUT_PATH)));
+            String line1;
+            while ((line1 = reader1.readLine()) != null) {
+                String[] wordsInline = line1.split("\t");
+                if (wordsInline.length >= 2) {
+                    Word new_word = new Word(wordsInline[0], wordsInline[1]);
+                    oldWord.add(new_word);
+                }
+            }
+
             BufferedReader reader = new BufferedReader(new FileReader(new File(INPUT_PATH)));
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] wordsInline = line.split("\t");
-
-                Word new_word = new Word(wordsInline[0], wordsInline[1]);
-                oldWord.add(new_word);
+                if (wordsInline.length >= 2) {
+                    Word new_word = new Word(wordsInline[0], wordsInline[1]);
+                    oldWord.add(new_word);
+                }
             }
             Collections.sort(oldWord);
             removeDuplicates();
+            updateWordtoFile();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (Exception e) {
@@ -107,12 +122,14 @@ public class DictionaryManagement extends Dictionary{
             }
             bufferedWriter.flush();
             bufferedWriter.close();
-        } catch (Exception e) {
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     /**
+     * Custom binary search.
      * check if the word is in the dictionary.
      * it returns the position of the word.
      * in case mid_pos = 0, the word will be in 0, it returns-1 when word not in the dic.
@@ -162,28 +179,36 @@ public class DictionaryManagement extends Dictionary{
     public static void addWord(String input_word, String input_meaning) {
         input_word = input_word.toLowerCase();
         input_meaning = input_meaning.toLowerCase();
-        int posInputWord = binaryGetPos(0, oldWord.size(), input_word);
-        if (posInputWord == -1) {
-            System.out.println("The word existed");
-            return;
+        if (oldWord.size() > 2) {
+            int posInputWord = binaryGetPos(0, oldWord.size(), input_word);
+            if (posInputWord == -1) {
+                System.out.println("The word existed");
+                return;
+            }
+            oldWord.add(new Word());
+            for (int i = oldWord.size() - 2; i >= posInputWord; i--) {
+                oldWord.get(i + 1).setSearching(oldWord.get(i).getSearching());
+                oldWord.get(i + 1).setMeaning(oldWord.get(i).getMeaning());
+            }
+            oldWord.get(posInputWord).setSearching(input_word);
+            oldWord.get(posInputWord).setMeaning(input_meaning);
+        } else {
+            Word word = new Word(input_word, input_meaning);
+            oldWord.add(word);
         }
-        oldWord.add(new Word());
-        for (int i = oldWord.size() - 2; i >= posInputWord; i--) {
-            oldWord.get(i + 1).setSearching(oldWord.get(i).getSearching());
-            oldWord.get(i + 1).setMeaning(oldWord.get(i).getMeaning());
-        }
-        oldWord.get(posInputWord).setSearching(input_word);
-        oldWord.get(posInputWord).setMeaning(input_meaning);
+        Collections.sort(oldWord);
         updateWordtoFile();
     }
 
+    /**
+     * change a word's meaning.
+     */
     public static void alterWord(String altered_word, String altered_meaning) {
         altered_word = altered_word.toLowerCase();
         altered_meaning = altered_meaning.toLowerCase();
-
-        int check = binaryGetPos(0, oldWord.size(), altered_word);
-        if (check == -1) {
-            int pos = Collections.binarySearch(oldWord, new Word(altered_word, null));
+        int pos = -1;
+        pos = Collections.binarySearch(oldWord, new Word(altered_word, null));
+        if (pos >= 0) {
             oldWord.get(pos).setMeaning(altered_meaning);
         } else {
             System.out.println("Can't find the word you want to change");
@@ -191,20 +216,25 @@ public class DictionaryManagement extends Dictionary{
         updateWordtoFile();
     }
 
+    /**
+     * remove a word from dictionary.
+     */
     public static void removeWord(String deleted_word) {
         deleted_word = deleted_word.toLowerCase();
-        int check = binaryGetPos(0, oldWord.size(), deleted_word);
-        if (check == -1) {
-            int pos = Collections.binarySearch(oldWord, new Word(deleted_word, null));
-            oldWord.remove(pos);
+        int index = Collections.binarySearch(oldWord, new Word(deleted_word, null));
+        System.out.println(index);
+        if (index >= 0) {
+            oldWord.remove(oldWord.get(index));
             System.out.println("Deleted success");
         } else {
-            System.out.println("Can't the word you want to remove");
+            System.out.println("Can't find the word you want to remove");
         }
         updateWordtoFile();
     }
 
-    // find words that have the same element
+    /**
+     * find words that have similar letter.
+     */
     public List<Word> dictionarySearcher(String letter) {
         List<Word> foundWords = new ArrayList<>();
         for(Word word : oldWord) {
@@ -215,76 +245,11 @@ public class DictionaryManagement extends Dictionary{
         return foundWords;
     }
 
-    public static int isContain(String s1, String s2) {
-        for(int i = 0; i < Math.min(s1.length(),s2.length()); i++) {
-            if (s1.charAt(i) > s2.charAt(i)) {
-                return 1;
-            } else if (s1.charAt(i) < s2.charAt(i)) {
-                return -1;
-            }
-        }
-        if (s1.length() > s2.length()) {
-            return 1;
-        }
-        return 0;
-    }
-
-    // search word position in dictionary
-    public static int binaryLookup(int start, int end, String word) {
-        if (end < start) return -1;
-        int mid = start + (end - start) / 2;
-        int compare = isContain(word, oldWord.get(mid).getSearching());
-        // the word is on the left side
-        if (compare == -1) return binaryLookup(start, mid - 1, word);
-            // the word is on the right side
-        else if (compare == 1) {
-            return binaryLookup(mid + 1, end, word);
-        }
-        // the word is in the middle
-        else return mid;
-    }
-//
-//    public static void showWordLookup(String word, int index) {
-//        // the word not in the dictionary
-//        if (index < 0) return;
-//
-//        ArrayList<Word> listWord = new ArrayList<>();
-//        int j = index;
-//        while (j >= 0) {
-//            if (isContain(word,oldWord.get(j).getSearching()) == 0) {
-//                j--;
-//            } else break;
-//        }
-//
-//        for (int i = j + 1; i < index; i++) {
-//            Word tmp = new Word(oldWord.get(i).getSearching(), oldWord.get(i).getMeaning());
-//            listWord.add(tmp);
-//        }
-//
-//        for (int i = index + 1; i < oldWord.size(); i++) {
-//            if (isContain(word, oldWord.get(i).getSearching()) == 0) {
-//                Word tmp = new Word(oldWord.get(i).getSearching(), oldWord.get(i).getMeaning());
-//                listWord.add(tmp);
-//            } else break;
-//        }
-//
-//        for (Word w : listWord) {
-//            System.out.println(w.getSearching());
-//        }
-//    }
-//
-//    public static void LookUpWord() throws IOException {
-//        Scanner sc = new Scanner(System.in);
-//        String w = sc.nextLine().toLowerCase();
-//        int index = binaryLookup(0, oldWord.size(), w);
-//        if (index < 0) {
-//            index = binaryLookup(0, oldWord.size(), w);
-//        }
-//        showWordLookup(w,index);
-//    }
-
+    /**
+     * show word's meaning
+     */
     public static String findVietnameseMeaning(String word) {
-        int index = binaryLookup(0, oldWord.size(), word.toLowerCase());
+        int index = Collections.binarySearch(oldWord, new Word(word, null));
         if (index == -1) {
             return "Can't find the word you want to search!";
         }
